0d34cd20245e35bb99d73d20ae378bdb36b510bc
isaacGrodriguez <falcohalcon@gmail.com>
2022-09-03 16:31:10 -0500

    Audio Test 2.0

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function PlaySound{
+// var audio = new Audio('./audio/selectaudio.mp3');
+// audio.loop = false;
+// audio.play(); 
+// }
+
+// const playAudio = document.getElemntById('playAudio');
+// playAudio.addEventListener('click', PlaySound);
+
+
+
+// document.getElementById("playAudio").addEventListener("click", function(){
+// 	var audio = document.getElementById('selectaudio');
+//   if(this.className = 'is-playing'){
+//     this.className = "rpgui-button beginButton";
+//     audio.play();
+//   }
+
+// });
