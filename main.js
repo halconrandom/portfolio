46f2b32ab33ec38a0461f5c159edc536b3bf06a9
isaacGrodriguez <falcohalcon@gmail.com>
2022-09-05 13:40:50 -0500

    Github Update

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var contador = 0;
 var textBox = document.getElementById('textBox');
-var mql = window.matchMedia("screen and (max-width: 1280px)")
+var mql = window.matchMedia("(max-width: 1280px)")
 
 function change_text(){
-    if (contador <= 9) {
+    if (contador <= 30) {
         document.getElementById('clickSound').play();
         contador = contador+1;
 
@@ -47,28 +47,62 @@ function change_text(){
             document.getElementById('buttonSeleccion');
             buttonSeleccion.style.display = "flex"
         }
-        
     };
 
+}
+
+var contadorGithub = 0;
+var textBoxGithub = document.getElementById('textBoxGithub0');
+var mql = window.matchMedia("(max-width: 1280px)")
+
+function change_text2(){
+    if (contadorGithub <= 10) {
+        document.getElementById('clickSound').play();
+        contadorGithub = contadorGithub+1;
+
+        if (contadorGithub === 1) {
+            document.getElementById('textBoxGithub1');
+            textBoxGithub0.style.display ="none"
+            textBoxGithub1.style.display = "flex"
+
+        } else if (contadorGithub === 2) {
+            document.getElementById('textBoxGithub2');
+            textBoxGithub1.style.display ="none"
+            textBoxGithub2.style.display = "flex"
+        } else if (contadorGithub === 3) {
+            document.getElementById('textBoxGithub3');
+            textBoxGithub2.style.display ="none"
+            textBoxGithub3.style.display = "flex"
+        } else if (contadorGithub === 4) {
+            document.getElementById('textBoxGithub4');
+            textBoxGithub3.style.display ="none"
+            textBoxGithub4.style.display = "flex"
+        } else if (contadorGithub === 5) {
+            document.getElementById('textBoxGithub5');
+            textBoxGithub4.style.display ="none"
+            textBoxGithub5.style.display = "flex"
+            document.getElementById('githubtrolButton');
+            githubtrolButton.style.display = "flex"
+        } else if (contadorGithub === 6) {
+            document.getElementById('textBoxGithub6');
+            textBoxGithub5.style.display ="none"
+            githubtrolButton.style.display = "none"
+            textBoxGithub6.style.display = "flex"
+        } else if (contadorGithub === 7) {
+            document.getElementById('textBoxGithub7');
+            textBoxGithub6.style.display ="none"
+            document.getElementById('githubButton');
+            githubButton.style.display = "flex"
+            textBoxGithub7.style.display = "flex"
+        } else if (contadorGithub === 8) {
+            document.getElementById('textBoxGithub8');
+            textBoxGithub7.style.display ="none"
+            textBoxGithub8.style.display = "flex"
+        }
+    }   
 }
 
 
 
 
 
-
-
-
-
-// function optionNegativo(){
-//     var contadorNegativo = 0;
-//     document.getElementById('clickSound').play();
-//     squareTextWelcome.style.display = "flex";
-//     seleccionLista.style.display = "none";
-//     if (contadorNegativo <= 3){
-//         document.getElementById('clickSound').play();
-//         contadorNegativo = contadorNegativo+1
-//         if (contadorNegativo === 1){
-//             textBox.innerHTML = 'Oh, well sometimes life is pretty harsh. But lets hope you can get better tomorrow.';
-//         }
-//     }
